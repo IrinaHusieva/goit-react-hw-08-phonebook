ed63a04a395214ce8c79dd086c8d4fe3b7b6a4a9
Iryna Husieva <02guseva02@gmail.com>
2023-09-09 15:21:33 +0200

    private routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,19 @@ import LoginForm from './Forms/LoginForm';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Contacts } from 'pages/Contacts';
 import Header from './Header';
+import PrivateRoute from 'Routes/PrivateRoute';
 
 export const App = () => {
   
-  return (
+   return (
     <Router>
-      <Header></Header>
-        <Routes>
-          <Route path="/register" element={<RegistrForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/*" element={<Navigate to="/contacts" />} /> 
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/register" element={<RegistrForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/contacts" element={<PrivateRoute redirectTo="/login"component={Contacts} />} />
+        <Route path="/*" element={<Navigate to="/contacts" />} />
+      </Routes>
     </Router>
   );
 };
