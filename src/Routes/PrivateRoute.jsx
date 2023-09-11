40b0ae2efd1282a026e7af3da28caea6f3b574c7
Iryna Husieva <02guseva02@gmail.com>
2023-09-11 16:21:24 +0200

    Update PrivateRoute.jsx

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -6,3 +6,19 @@ const PrivateRoute = ({ children }) => {
 }
 
 export default PrivateRoute
+// import React from 'react';
+// import { Route, Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { selectIsLoggedIn } from 'redux/auth/selectors';
+
+// const PrivateRoute = ({ element }) => {
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+
+//   if (isLoggedIn) {
+//     return <Route element={element} />;
+//   } else {
+//     return <Navigate to="/login" />;
+//   }
+// };
+
+// export default PrivateRoute;
