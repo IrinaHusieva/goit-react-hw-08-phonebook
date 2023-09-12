216dc3581c1a5f2e0c8f8250f6413eff6b60d445
Iryna Husieva <02guseva02@gmail.com>
2023-09-12 10:26:12 +0200

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,38 @@
 
-import RegistrForm from './Forms/RegistrForm';
-import LoginForm from './Forms/LoginForm';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import RegistrForm from './Forms/RegistrForm';
+// import LoginForm from './Forms/LoginForm';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import { Contacts } from 'pages/Contacts';
+// import Navigation from './Navigation';
+// import PrivateRoute from 'Routes/PrivateRoute';
+
+// export const App = () => {
+//   return (
+//     <Router>
+//       <Navigation />
+//       <Routes>
+//         <Route path="/register" element={<RegistrForm />} />
+//         <Route path="/login" element={<LoginForm />} />
+//         <Route
+//           path="/contacts"
+//           element={
+//             <PrivateRoute redirectTo="/login">
+//               <Contacts />
+//             </PrivateRoute>
+//           }
+//         />
+//         <Route path="/*" element={<Navigate to="/login" />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Contacts } from 'pages/Contacts';
 import Navigation from './Navigation';
-import PrivateRoute from 'Routes/PrivateRoute';
+import  RegistrForm  from '../components/Forms/RegistrForm';
+import  LoginForm  from '../components/Forms/LoginForm';
+import {PrivateRoute}  from '../Routes/PrivateRoute';
+import  RestrictedRoute  from '../Routes/RestrictedRoute'
 
 export const App = () => {
   return (
@@ -15,13 +43,12 @@ export const App = () => {
         <Route path="/login" element={<LoginForm />} />
         <Route
           path="/contacts"
-          element={
-            <PrivateRoute redirectTo="/login">
-              <Contacts />
-            </PrivateRoute>
-          }
+          element={<PrivateRoute component={<Contacts />} redirectTo="/login" />}
         />
-        <Route path="/*" element={<Navigate to="/login" />} />
+        <Route
+          path="/restricted" 
+          element={<RestrictedRoute component={<Contacts />} redirectTo="/login" />}
+        />
       </Routes>
     </Router>
   );
--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,15 +1,24 @@
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/selectors';
 import { logOut } from 'redux/auth/operations';
+import { useNavigate } from 'react-router-dom';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
+  const navigate = useNavigate();
 
- return (
+  const handleLogout = () => {
+    dispatch(logOut());
+    localStorage.removeItem('authToken');
+    navigate('/login');
+  };
+
+  return (
     <div>
       <p>{user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+      <button type="button" onClick={handleLogout}>
         Logout
       </button>
     </div>
